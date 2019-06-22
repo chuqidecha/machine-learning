1098fed840b8b88c25ffe16f6e37d37ba890d7e7
yinwb <yinwb91@163.com>
2019-06-22 22:36:06 +0800

    kaggle:bike:lightgbm'

--- kaggle/bike-rental-predict/lightgbm.py
+++ kaggle/bike-rental-predict/lightgbm.py
@@ -13,8 +13,6 @@ data_train.shape
 #%%
 data_train.head(3)
 #%%
-data_train.isnull().values.any()
-#%%
 data_train.datetime = data_train.datetime.apply(pd.to_datetime)
 data_train['month'] = data_train.datetime.dt.month
 data_train['hour'] = data_train.datetime.dt.hour
@@ -29,41 +27,61 @@ print(list(zip(X_train.columns,X_train.dtypes)))
 #%%
 X_train.head(3)
 #%%
-lightgbm = lgb.LGBMRegressor(objective='regression_l2',
-                       boosting='gbdt',
-                       metric='root_mean_squared_error',
-                       max_depth=8,
-                       bagging_fraction=0.8,
-                       bagging_freq=5, 
-                       bagging_seed=1234,
-                       categorical_feature='0,1,2,3,4,5,6',
-                       random_state=1234)
-#%%
-kf = KFold(n_splits=5, random_state=1234, shuffle=True)
+def objective_func(train_set,num_boost_round=10000,nfold=5,stratified=False,shuffle=True,early_stopping_rounds=50,seed=1234):
+    def objective(params):
+        ret = lgb.cv(params,dataset_train,num_boost_round=10000,nfold=5,stratified=False,shuffle=True,early_stopping_rounds=50,seed=1234)
+        return ret['rmse-mean'][-1]
+    return objective
 
-#%%
-dataset_train = lgb.Dataset(X_train,y_train)
-params={
+space={
     'objective':'regression_l2',
     'boosting':'gbdt',
     'metric':'root_mean_squared_error',
-    'max_depth':5,
+    'max_depth':hp.choice('max_depth', np.linspace(4,10,7).astype(np.int)),
+    'num_leaves': hp.choice('num_leaves', np.linspace(32,128,49).astype(np.int)),
     'bagging_fraction':0.8,
     'bagging_freq':5, 
     'bagging_seed':1234,
     'categorical_feature':[0,1,2,3,4,5,6],
-    'random_state':1234
+    'feature_fraction':0.8,
+    'learning_rate': hp.uniform('learning_rate',0,0.3),
+    'random_state':1234,
+    'lambda_l2':hp.uniform('lambda_l2',0,20)
 }
-ret = lgb.cv(params,dataset_train,num_boost_round=10000,nfold=5,stratified=False,shuffle=True,early_stopping_rounds=50,seed=1234)
+dataset_train = lgb.Dataset(X_train, y_train)
+best = fmin(fn=objective_func(dataset_train,num_boost_round=10000,nfold=5,stratified=False,shuffle=True,early_stopping_rounds=50,seed=1234),
+            space=space,
+            algo=tpe.suggest,
+            max_evals=300)
+#%%
+print(best)
 
 #%%
-print('num_boost_round = %s, best_iter = %s' %(len(ret['rmse-mean']),np.argmin(ret['rmse-mean'])))
+space={
+    'objective':'regression_l2',
+    'boosting':'gbdt',
+    'metric':'root_mean_squared_error',
+    'bagging_fraction':0.8,
+    'bagging_freq':5, 
+    'bagging_seed':1234,
+    'categorical_feature':[0,1,2,3,4,5,6],
+    'feature_fraction':0.8,
+    'learning_rate': hp.uniform('learning_rate',0,1),
+    'random_state':1234,
+    'lambda_l2':hp.uniform('lambda_l2',0,20)
+}
+space.update(best)
+print(space)
 #%%
-lightgbm = lgb.train(params,dataset_train,num_boost_round=len(ret['rmse-mean']))
+X_train_, X_test_, y_train_, y_test_ = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+dataset_train = lgb.Dataset(X_train_,y_train_)
+dataset_test = lgb.Dataset(X_test_,y_test_)
+lightgbm = lgb.train(space,dataset_train,num_boost_round=10000,valid_sets=dataset_test,early_stopping_rounds=50)
+y_train_predicted = lightgbm.predict(X_train)
+print("RMSLE on train: ", np.sqrt(mean_squared_error(y_train_predicted, y_train)))
+#%%
+print(list(zip(lightgbm.feature_importance(),lightgbm.feature_name())))
 
-#%%
-y_train_predicted = lightgbm.predict(X_train,np.argmin(ret))
-print("RMSLE on train: ", np.sqrt(metrics.mean_squared_error(y_train_predicted, y_train)))
 #%%
 data_test = pd.read_csv("./kaggle/bike-rental-predict/input/test.csv")
 data_test_ids = data_test["datetime"]
@@ -76,7 +94,7 @@ data_test['hour'] = data_test.datetime.dt.hour
 data_test['dayofweek'] = data_test.datetime.dt.dayofweek
 data_test.head()
 #%%
-X_test = data_test.drop(['datetime'], axis = 1)
+X_test = data_test[['season', 'holiday', 'workingday', 'weather','month','hour','dayofweek','temp','atemp','humidity','windspeed']]
 #%%
 y_test_predicted = lightgbm.predict(X_test)
 y_test_predicted = np.exp(y_test_predicted) - 1
@@ -88,5 +106,3 @@ submission = pd.DataFrame({
 submission.head()
 #%%
 submission.to_csv('./kaggle/bike-rental-predict/input/bike_predictions.csv', index=False)
-
-#%%
