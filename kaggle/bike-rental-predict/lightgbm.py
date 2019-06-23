5737673f7510bd29aa98040baca605b0e7ccae21
yinwb <yinwb91@163.com>
2019-06-23 23:25:20 +0800

    kaggle:bike:lightgbm

--- kaggle/bike-rental-predict/lightgbm.py
+++ kaggle/bike-rental-predict/lightgbm.py
@@ -2,12 +2,12 @@
 import numpy as np 
 import pandas as pd
 
-from sklearn.model_selection import cross_validate,KFold
+from sklearn.model_selection import cross_validate,KFold,train_test_split
 from sklearn.metrics import make_scorer,mean_squared_error 
 
 import lightgbm as lgb
 
-from hyperopt import hp, tpe, space_eval,STATUS_OK
+from hyperopt import hp, tpe, space_eval,STATUS_OK,Trials
 from hyperopt.fmin import fmin
 
 import warnings
@@ -26,18 +26,15 @@ def objective_fun(dataset_train, model_params={}, fit_params={}):
     def lgbm(params):
         model_params_copy = model_params.copy()
         model_params_copy.update(params)
-        print(model_params_copy)
         ret = lgb.cv(model_params_copy,dataset_train,**fit_params)
-        print(ret)
-        best_rounds = np.argmin(ret['rsme-mean'])
+        best_rounds = np.argmin(ret['rmse-mean'])
         return {
-            'loss': ret['rsme-mean'][best_rounds],
+            'loss': ret['rmse-mean'][best_rounds],
             'status': STATUS_OK,
-            'loss_variance':ret['rsme-std'][best_rounds],
+            'loss_variance':ret['rmse-stdv'][best_rounds],
             'best_rounds': best_rounds
         }
     return lgbm
-
 #%%
 model_params = {
     'objective':'regression_l2',
@@ -64,56 +61,37 @@ fit_params = {
 space={
     'max_depth': hp.choice('max_depth', np.linspace(4,10,7).astype(np.int)),
     'num_leaves': hp.choice('num_leaves', np.linspace(32,128,49).astype(np.int)),
-    'learning_rate': hp.uniform('learning_rate',0,0.3),
+    'learning_rate': hp.choice('learning_rate',[0.01,0.1,0.2]),
     'lambda_l2': hp.uniform('lambda_l2',0,20)
 }
+trials = Trials()
 dataset_train = lgb.Dataset(X_train, y_train)
 best = fmin(fn=objective_fun(dataset_train,model_params,fit_params),
             space=space,
             algo=tpe.suggest,
+            trials=trials,
             max_evals=300)
 #%%
-print(best)
-
-#%%
-space={
-    'objective':'regression_l2',
-    'boosting':'gbdt',
-    'metric':'root_mean_squared_error',
-    'bagging_fraction':0.8,
-    'bagging_freq':5, 
-    'bagging_seed':1234,
-    'categorical_feature':[0,1,2,3,4,5,6],
-    'feature_fraction':0.8,
-    'learning_rate': hp.uniform('learning_rate',0,1),
-    'random_state':1234,
-    'lambda_l2':hp.uniform('lambda_l2',0,20)
-}
-space.update(best)
-print(space)
+print(space_eval(space,best))
 #%%
 X_train_, X_test_, y_train_, y_test_ = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 dataset_train = lgb.Dataset(X_train_,y_train_)
 dataset_test = lgb.Dataset(X_test_,y_test_)
-lightgbm = lgb.train(space,dataset_train,num_boost_round=10000,valid_sets=dataset_test,early_stopping_rounds=50)
+params = model_params.copy()
+params.update(space_eval(space,best))
+lightgbm = lgb.train(params,dataset_train,num_boost_round=1000,valid_sets=dataset_test,early_stopping_rounds=50)
 y_train_predicted = lightgbm.predict(X_train)
 print("RMSLE on train: ", np.sqrt(mean_squared_error(y_train_predicted, y_train)))
 #%%
 print(list(zip(lightgbm.feature_importance(),lightgbm.feature_name())))
 
 #%%
-data_test = pd.read_csv("./kaggle/bike-rental-predict/input/test.csv")
+data_test = pd.read_csv("./kaggle/bike-rental-predict/input/test_feature.csv")
 data_test_ids = data_test["datetime"]
 data_test.head()
 
 #%%
-data_test.datetime = data_test.datetime.apply(pd.to_datetime)
-data_test['month'] = data_test.datetime.dt.month
-data_test['hour'] = data_test.datetime.dt.hour
-data_test['dayofweek'] = data_test.datetime.dt.dayofweek
-data_test.head()
-#%%
-X_test = data_test[['season', 'holiday', 'workingday', 'weather','month','hour','dayofweek','temp','atemp','humidity','windspeed']]
+X_test = data_test[use_cols]
 #%%
 y_test_predicted = lightgbm.predict(X_test)
 y_test_predicted = np.exp(y_test_predicted) - 1
