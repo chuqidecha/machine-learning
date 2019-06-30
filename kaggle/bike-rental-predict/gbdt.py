f6ec16b9fde241241e73d595bfacef920df88a9f
yinwb <yinwb91@163.com>
2019-06-30 08:29:20 +0800

    kaggle:format

--- kaggle/bike-rental-predict/gbdt.py
+++ kaggle/bike-rental-predict/gbdt.py
@@ -1,105 +1,114 @@
-#%%
-import numpy as np 
+# %%
+import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import cross_validate,KFold,train_test_split
-from sklearn.metrics import make_scorer,mean_squared_error 
+from sklearn.model_selection import cross_validate, KFold, train_test_split
+from sklearn.metrics import make_scorer, mean_squared_error
 
 import lightgbm as lgb
 
-from hyperopt import hp, tpe, space_eval,STATUS_OK,Trials
+from hyperopt import hp, tpe, space_eval, STATUS_OK, Trials
 from hyperopt.fmin import fmin
 
 import warnings
 warnings.filterwarnings("ignore")
-#%%
-use_cols=['season', 'holiday', 'workingday', 'weather','temp','atemp','humidity','windspeed','month','hour','dayofweek']
-data_train = pd.read_csv('./kaggle/bike-rental-predict/input/train_feature.csv')
+# %%
+use_cols = ['season', 'holiday', 'workingday', 'weather', 'temp',
+            'atemp', 'humidity', 'windspeed', 'month', 'hour', 'dayofweek']
+data_train = pd.read_csv(
+    './kaggle/bike-rental-predict/input/train_feature.csv')
 data_train.shape
-#%%
+# %%
 data_train.head(3)
-#%%
+# %%
 y_train = data_train['y'].values
 X_train = data_train[use_cols]
-#%%
+# %%
+
+
 def objective_fun(dataset_train, model_params={}, fit_params={}):
     def lgbm(params):
         model_params_copy = model_params.copy()
         model_params_copy.update(params)
-        ret = lgb.cv(model_params_copy,dataset_train,**fit_params)
+        ret = lgb.cv(model_params_copy, dataset_train, **fit_params)
         best_rounds = np.argmin(ret['rmse-mean'])
         return {
             'loss': ret['rmse-mean'][best_rounds],
             'status': STATUS_OK,
-            'loss_variance':ret['rmse-stdv'][best_rounds],
+            'loss_variance': ret['rmse-stdv'][best_rounds],
             'best_rounds': best_rounds
         }
     return lgbm
-#%%
+
+
+# %%
 model_params = {
-    'objective':'regression_l2',
-    'boosting':'gbdt',
-    'metric':'root_mean_squared_error',
-    'bagging_fraction':0.8,
-    'bagging_freq':5, 
-    'bagging_seed':1234,
-    'feature_fraction':0.8,
-    'categorical_feature':[0,1,2,3,4,5,6],
-    'random_state':1234
+    'objective': 'regression_l2',
+    'boosting': 'gbdt',
+    'metric': 'root_mean_squared_error',
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'bagging_seed': 1234,
+    'feature_fraction': 0.8,
+    'categorical_feature': [0, 1, 2, 3, 4, 5, 6],
+    'random_state': 1234
 }
 
 
 fit_params = {
-    'num_boost_round':1000,
-    'nfold':5,
-    'stratified':False,
-    'shuffle':True,
-    'early_stopping_rounds':50,
-    'seed':1234
+    'num_boost_round': 1000,
+    'nfold': 5,
+    'stratified': False,
+    'shuffle': True,
+    'early_stopping_rounds': 50,
+    'seed': 1234
 }
 
-space={
-    'max_depth': hp.choice('max_depth', np.linspace(4,10,7).astype(np.int)),
-    'num_leaves': hp.choice('num_leaves', np.linspace(32,128,49).astype(np.int)),
-    'learning_rate': hp.choice('learning_rate',[0.01,0.1,0.2]),
-    'lambda_l2': hp.uniform('lambda_l2',0,20)
+space = {
+    'max_depth': hp.choice('max_depth', np.linspace(4, 10, 7).astype(np.int)),
+    'num_leaves': hp.choice('num_leaves', np.linspace(32, 128, 49).astype(np.int)),
+    'learning_rate': hp.choice('learning_rate', [0.01, 0.1, 0.2]),
+    'lambda_l2': hp.uniform('lambda_l2', 0, 20)
 }
 trials = Trials()
 dataset_train = lgb.Dataset(X_train, y_train)
-best = fmin(fn=objective_fun(dataset_train,model_params,fit_params),
+best = fmin(fn=objective_fun(dataset_train, model_params, fit_params),
             space=space,
             algo=tpe.suggest,
             trials=trials,
             max_evals=300)
-#%%
-print(space_eval(space,best))
-#%%
-X_train_, X_test_, y_train_, y_test_ = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-dataset_train = lgb.Dataset(X_train_,y_train_)
-dataset_test = lgb.Dataset(X_test_,y_test_)
+# %%
+print(space_eval(space, best))
+# %%
+X_train_, X_test_, y_train_, y_test_ = train_test_split(
+    X_train, y_train, test_size=0.2, random_state=42)
+dataset_train = lgb.Dataset(X_train_, y_train_)
+dataset_test = lgb.Dataset(X_test_, y_test_)
 params = model_params.copy()
-params.update(space_eval(space,best))
-lightgbm = lgb.train(params,dataset_train,num_boost_round=1000,valid_sets=dataset_test,early_stopping_rounds=50)
+params.update(space_eval(space, best))
+lightgbm = lgb.train(params, dataset_train, num_boost_round=1000,
+                     valid_sets=dataset_test, early_stopping_rounds=50)
 y_train_predicted = lightgbm.predict(X_train)
 print("RMSLE on train: ", np.sqrt(mean_squared_error(y_train_predicted, y_train)))
-#%%
-print(list(zip(lightgbm.feature_importance(),lightgbm.feature_name())))
+# %%
+print(list(zip(lightgbm.feature_importance(), lightgbm.feature_name())))
 
-#%%
+# %%
 data_test = pd.read_csv("./kaggle/bike-rental-predict/input/test_feature.csv")
 data_test_ids = data_test["datetime"]
 data_test.head()
 
-#%%
+# %%
 X_test = data_test[use_cols]
-#%%
+# %%
 y_test_predicted = lightgbm.predict(X_test)
 y_test_predicted = np.exp(y_test_predicted) - 1
-#%%
+# %%
 submission = pd.DataFrame({
-        "datetime": data_test_ids,
-        "count": np.rint(y_test_predicted).astype(np.int)
-    })
+    "datetime": data_test_ids,
+    "count": np.rint(y_test_predicted).astype(np.int)
+})
 submission.head()
-#%%
-submission.to_csv('./kaggle/bike-rental-predict/input/bike_predictions.csv', index=False)
+# %%
+submission.to_csv(
+    './kaggle/bike-rental-predict/input/bike_predictions.csv', index=False)
--- kaggle/bike-rental-predict/lr.py
+++ kaggle/bike-rental-predict/lr.py
@@ -2,89 +2,99 @@
 线性回归
 """
 
-#%%
-import numpy as np 
-import pandas as pd 
-from sklearn.model_selection import GridSearchCV,KFold
-from sklearn import linear_model,metrics,pipeline, preprocessing
+# %%
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn import linear_model, metrics, pipeline, preprocessing
 import warnings
 warnings.filterwarnings("ignore")
-#%%
-use_cols=['season', 'holiday', 'workingday', 'weather','temp','atemp','humidity','windspeed','month','hour','dayofweek']
-data_train = pd.read_csv('./kaggle/bike-rental-predict/input/train_feature.csv')
+# %%
+use_cols = ['season', 'holiday', 'workingday', 'weather', 'temp',
+            'atemp', 'humidity', 'windspeed', 'month', 'hour', 'dayofweek']
+data_train = pd.read_csv(
+    './kaggle/bike-rental-predict/input/train_feature.csv')
 data_train.shape
-#%%
+# %%
 data_train.head(3)
-#%%
+# %%
 y_train = data_train['y'].values
 X_train = data_train[use_cols]
-#%%
+# %%
 binary_data_columns = ['holiday', 'workingday']
-binary_data_indices = np.array([(column in binary_data_columns) for column in X_train.columns], dtype = bool)
+binary_data_indices = np.array(
+    [(column in binary_data_columns) for column in X_train.columns], dtype=bool)
 
-categorical_data_columns = ['season', 'weather', 'month', 'hour', 'dayofweek'] 
-categorical_data_indices = np.array([(column in categorical_data_columns) for column in X_train.columns], dtype = bool)
+categorical_data_columns = ['season', 'weather', 'month', 'hour', 'dayofweek']
+categorical_data_indices = np.array(
+    [(column in categorical_data_columns) for column in X_train.columns], dtype=bool)
 
 numeric_data_columns = ['temp', 'atemp', 'humidity', 'windspeed']
-numeric_data_indices = np.array([(column in numeric_data_columns) for column in X_train.columns], dtype = bool)
-#%%
-transformer_list = [        
-            #binary
-            ('binary_variables_processing', preprocessing.FunctionTransformer(lambda data: data[:, binary_data_indices])), 
-                    
-            #numeric
-            ('numeric_variables_processing', pipeline.Pipeline(steps = [
-                    ('selecting', preprocessing.FunctionTransformer(lambda data: data[:, numeric_data_indices])),
-                    ('scaling', preprocessing.StandardScaler(with_mean = 0)) 
-                ])
-            ),
-            #categorical
-            ('categorical_variables_processing', pipeline.Pipeline(steps = [
-                    ('selecting', preprocessing.FunctionTransformer(lambda data: data[:, categorical_data_indices])),
-                    ('hot_encoding', preprocessing.OneHotEncoder(handle_unknown = 'ignore'))
-                ])
-            ),
-        ]
-feature_encode = pipeline.Pipeline(steps = [       
+numeric_data_indices = np.array(
+    [(column in numeric_data_columns) for column in X_train.columns], dtype=bool)
+# %%
+transformer_list = [
+    # binary
+    ('binary_variables_processing', preprocessing.FunctionTransformer(
+        lambda data: data[:, binary_data_indices])),
+
+    # numeric
+    ('numeric_variables_processing', pipeline.Pipeline(steps=[
+        ('selecting', preprocessing.FunctionTransformer(
+            lambda data: data[:, numeric_data_indices])),
+        ('scaling', preprocessing.StandardScaler(with_mean=0))
+    ])
+    ),
+    # categorical
+    ('categorical_variables_processing', pipeline.Pipeline(steps=[
+        ('selecting', preprocessing.FunctionTransformer(
+            lambda data: data[:, categorical_data_indices])),
+        ('hot_encoding', preprocessing.OneHotEncoder(handle_unknown='ignore'))
+    ])
+    ),
+]
+feature_encode = pipeline.Pipeline(steps=[
     ('feature_processing', pipeline.FeatureUnion(transformer_list=transformer_list))]
 )
 
 X_train_encode = feature_encode.fit_transform(X_train)
-#%%
+# %%
 X_train_encode.shape
-#%%
+# %%
 data_test = pd.read_csv("./kaggle/bike-rental-predict/input/test_feature.csv")
 data_test.head()
-#%%
+# %%
 X_test = data_test[use_cols]
 id_test = data_test['datetime']
 
-#%%
+# %%
 X_test_encode = feature_encode.transform(X_test)
 X_test_encode.shape
-#%%
+# %%
 kf = KFold(n_splits=5, random_state=1234, shuffle=True)
-#%%
+# %%
 estimator = linear_model.Ridge(max_iter=3000)
 grid_search = GridSearchCV(estimator,
-    param_grid={'alpha':np.logspace(-8,8,17)},
-    scoring='neg_mean_squared_error',cv=kf) 
-grid_search.fit(X_train_encode,y_train)
+                           param_grid={'alpha': np.logspace(-8, 8, 17)},
+                           scoring='neg_mean_squared_error', cv=kf)
+grid_search.fit(X_train_encode, y_train)
 print("Best parameters:{}".format(grid_search.best_params_))
 print("Best best_score:{}".format(-grid_search.best_score_))
-#%%
+# %%
 pd.DataFrame(grid_search.cv_results_).head(20)
-#%%
-y_train_predicted =  grid_search.best_estimator_.predict(X_train_encode)
-print("RMSLE on train: ", np.sqrt(metrics.mean_squared_error(y_train_predicted, y_train)))
-#%%
+# %%
+y_train_predicted = grid_search.best_estimator_.predict(X_train_encode)
+print("RMSLE on train: ", np.sqrt(
+    metrics.mean_squared_error(y_train_predicted, y_train)))
+# %%
 y_test_predicted = grid_search.best_estimator_.predict(X_test_encode)
 y_test_predicted = np.exp(y_test_predicted) - 1
-#%%
+# %%
 submission = pd.DataFrame({
-        "datetime": id_test,
-        "count": np.rint(y_test_predicted).astype(np.int)
-    })
+    "datetime": id_test,
+    "count": np.rint(y_test_predicted).astype(np.int)
+})
 submission.head()
-#%%
-submission.to_csv('./kaggle/bike-rental-predict/input/bike_predictions.csv', index=False)
+# %%
+submission.to_csv(
+    './kaggle/bike-rental-predict/input/bike_predictions.csv', index=False)
